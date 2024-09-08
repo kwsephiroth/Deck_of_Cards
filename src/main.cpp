51ba43b74d120bf6f36a4aa29077152cce832838
Kenneth Walker <kwalkerdev@gmail.com>
2024-09-08 10:18:20 -0700

    Added some test logic.

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,29 @@
 int main()
 {
 	Seegrid::Poker::Deck deck;
-	for(;;)
+
+	auto deal_and_print_cards = [](Seegrid::Poker::Deck& deck) mutable
 	{
-		auto dealt_card = deck.deal_card();
-		if (dealt_card)
-			std::cout << *dealt_card << "\n";
-		else
-			break;
-	}
+		for (;;)
+		{
+			auto dealt_card = deck.deal_card();
+			if (dealt_card)
+				std::cout << *dealt_card << "\n";
+			else
+				break;
+		}
+	};
+
+	deal_and_print_cards(deck);
+	std::cout << '\n';
+	deck = Seegrid::Poker::Deck();
+	deck.shuffle();
+	std::cout << "\nSHUFFLED:\n";
+	deal_and_print_cards(deck);
+	deck = Seegrid::Poker::Deck();
+	deck.shuffle();
+	std::cout << "\nSHUFFLED:\n";
+	deal_and_print_cards(deck);
+
 	return 0;
 }
